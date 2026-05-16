5d0bec598a393a2a3bb07f7768604db05e58ed50
mauricio-gallera <galmeidamauricio12@gmail.com>
2026-05-16 03:39:02 -0300

    wip: change schema and add predictions routes

---
+++ app/api/predictions/route.ts
@@ -0,0 +1,77 @@
+import { auth } from "@/lib/auth"
+import { NextResponse } from "next/server"
+import { db } from "@/lib/db"
+import { POOL_LIMITS } from "@/lib/config"
+
+
+export async function POST(req: Request) {
+
+    const session = await auth();
+    const { round, matches } = await req.json()
+
+    if (!session?.user?.id) {
+        return Response.json({ error: "Unauthorized" }, { status: 401 })
+    }
+
+    const userId = session.user.id
+    const user   = await db.user.findUnique({ where: { id: userId } });
+
+    if (!user) {
+        return Response.json({ error: "User not found" }, { status: 404 })
+    }
+
+    const predictions = []
+    for (const match of matches) {
+        const prediction = { userId, matchId: match.id, predictedA: match.predictedA, predictedB: match.predictedB,round }
+        predictions.push(prediction)
+    }
+
+
+    const persistedPredictions = await db.prediction.createMany({data: predictions})
+
+    console.log("vai responder!")    
+
+    return NextResponse.json(
+        { predictions: persistedPredictions },
+        { status: 201 }
+    )
+}
+
+// export async function GET() {  
+
+//     const session = await auth();
+
+//     console.log("session:", session)
+
+//     if (!session?.user?.id) {
+//         return Response.json({ error: "Unauthorized" }, { status: 401 })
+//     }
+
+//     const userId = session.user.id
+
+//     const pools = await db.pool.findMany({
+//         where: {
+//             members: {
+//                 some: { userId }
+//             }
+//         },
+//         include: {
+//             members: {
+//                 select: { id: true }
+//             }
+//         }
+//     })
+
+//     const result = pools.map(pool => ({
+//         id: pool.id,
+//         name: pool.name,
+//         inviteCode: pool.inviteCode,
+//         isPrivate: pool.isPrivate,
+//         maxParticipants: pool.maxParticipants,
+//         memberCount: pool.members.length,
+//         isOwner: pool.ownerId === userId,
+//         createdAt: pool.createdAt,
+//     }))
+
+//     return Response.json(result)
+// }
